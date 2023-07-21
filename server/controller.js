b99b12a32ee9734858e6ac195e2fc00e999fd347
Victor Dai <vdsayf@gmail.com>
2023-07-21 08:35:03 +0800

    tested, deploying next

---
+++ server/controller.js
@@ -0,0 +1,31 @@
+const model = require('../db/model.js');
+
+exports.getMetaData = async (req,res) => {
+  let result = await model.getMeta(req.query.product_id);
+  res.status(200)
+  res.send(result)
+}
+
+exports.putHelpful = (req,res) => {
+  model.helpfulReview(req.params.review_id)
+  res.status(204)
+  res.send()
+}
+
+exports.putReport = (req,res) => {
+  model.reportReview(req.params.review_id)
+  res.status(204)
+  res.send()
+}
+
+exports.postReview = (req,res) => {
+  model.newReview(req.body);
+  res.status(201)
+  res.send()
+}
+
+exports.getReviewsData = async (req,res) => {
+  let result = await model.getReviews(req.query.product_id, req.query.page, req.query.count);
+  res.status(200)
+  res.send(result)
+}
